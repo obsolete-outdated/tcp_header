6e28ae02d1b406243c6307879aea37c7aa4cb219
Aleksei <54249358+Moonspeaker@users.noreply.github.com>
2019-12-27 15:26:24 +0300

    minor refactor

--- lib/tcp_header.dart
+++ lib/tcp_header.dart
@@ -3,13 +3,14 @@ library tcp_header;
 import 'dart:typed_data';
 
 const int _headerLength = 4;
+const int _byteArrayStart = 0;
 
-Uint8List dataWithTCPHeader(final Uint8List data) => Uint8List.fromList(((Uint8List(_headerLength)..buffer.asByteData().setUint32(0, data.length + _headerLength)).followedBy(data)).toList());
+Uint8List dataWithTCPHeader(final Uint8List data) => Uint8List.fromList(((Uint8List(_headerLength)..buffer.asByteData().setUint32(_byteArrayStart, data.length + _headerLength)).followedBy(data)).toList());
 
-bool dataWithTCPHeaderCanBeDecoded(final Uint8List dataWithTCPHeader) => dataWithTCPHeader.length>_headerLength && dataWithTCPHeader.length == dataWithTCPHeader.buffer.asByteData().getUint32(0);
+bool dataWithTCPHeaderCanBeDecoded(final Uint8List dataWithTCPHeader) => dataWithTCPHeader.length>_headerLength && dataWithTCPHeader.length == dataWithTCPHeader.buffer.asByteData().getUint32(_byteArrayStart);
 
 Uint8List dataFromDataWithTCPHeader(final Uint8List dataWithTCPHeader) => dataWithTCPHeader.sublist(_headerLength);
 
 bool headerCanBeExtracted(final Uint8List dataWithTCPHeader) => dataWithTCPHeader.length>=_headerLength;
 
-int lengthHeaderFromData(final Uint8List dataWithTCPHeader) => dataWithTCPHeader.sublist(0, 4).buffer.asByteData().getUint32(0);
+int lengthHeaderFromData(final Uint8List dataWithTCPHeader) => dataWithTCPHeader.sublist(_byteArrayStart, _headerLength).buffer.asByteData().getUint32(_byteArrayStart);
