78711000e6a392d1d7c34a5bf8e414a5d4ae8fe0
Aleksei <54249358+Moonspeaker@users.noreply.github.com>
2019-12-28 15:02:43 +0300

    added decoder

--- lib/tcp_header.dart
+++ lib/tcp_header.dart
@@ -14,3 +14,39 @@ Uint8List dataFromDataWithTCPHeader(final Uint8List dataWithTCPHeader) => dataWi
 bool headerCanBeExtracted(final Uint8List dataWithTCPHeader) => dataWithTCPHeader.length>=_headerLength;
 
 int lengthHeaderFromData(final Uint8List dataWithTCPHeader) => dataWithTCPHeader.sublist(_byteArrayStart, _headerLength).buffer.asByteData().getUint32(_byteArrayStart);
+
+class MessageDecoder{
+  static const int _maxMessageLength = 0xffffff;
+  static const bool _sanityUncheckedDefault = true;
+  static final Uint8List _dataDefault = Uint8List(0);
+
+  Uint8List _data = _dataDefault;
+  bool _sanityUnchecked = _sanityUncheckedDefault;
+
+  void _reset(){
+    _data = _dataDefault;
+    _sanityUnchecked = _sanityUncheckedDefault;
+  }
+
+  void addData(final Uint8List newData){
+    _data = Uint8List.fromList(_data.followedBy(newData).toList());
+
+    if(_sanityUnchecked){
+      if(headerCanBeExtracted(_data)){
+        if(lengthHeaderFromData(_data) > _maxMessageLength){
+          throw 'Message is too big';
+        } else {
+          _sanityUnchecked = false;
+        }
+      }
+    }
+  }
+
+  bool get canDecode => dataWithTCPHeaderCanBeDecoded(_data);
+
+  Uint8List get decode {
+    final Uint8List decodedData = dataFromDataWithTCPHeader(_data);
+    _reset();
+    return decodedData;
+  }
+}
